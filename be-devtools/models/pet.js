d8d78d3c1cc1a6b3e30f9a67781f56c0f608b0d8
Kpp7947 <katepapha7947@gmail.com>
2025-10-10 21:08:10 +0700

    add models

---
+++ be-devtools/models/pet.js
@@ -0,0 +1,32 @@
+const sequelize = require("../config/dbconn")
+const { DataTypes } = require("sequelize")
+
+const Pet = sequelize.define("Pet", {
+    name: {
+        type: DataTypes.STRING,
+    },
+    species: {
+        type: DataTypes.ENUM("cat", "dog")
+    },
+    breed: {
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+    },
+    gender: {
+        type: DataTypes.ENUM("male", "female"),
+    },
+    size: {
+        type: DataTypes.ENUM("small", "medium", "big"),
+    },
+    image: {
+        type: DataTypes.STRING,
+    },
+    status: {
+        type: DataTypes.ENUM('available', 'adopted', 'reserved'),
+        defaultValue: 'available'
+    },
+})
+
+module.exports = Pet
